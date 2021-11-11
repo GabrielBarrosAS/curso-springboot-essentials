51869df0f78620247ab87c99d61580ff52feeb00
Gabriel Barros <gabrielbarros201612@gmail.com>
2021-11-11 13:26:43 -0300

    Criando um novo registro via requisição post

--- src/main/java/lead/mentoring/springboot2/service/AnimeService.java
+++ src/main/java/lead/mentoring/springboot2/service/AnimeService.java
@@ -2,15 +2,22 @@ package lead.mentoring.springboot2.service;
 
 import lead.mentoring.springboot2.domain.Anime;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    private List<Anime> animes = List.of(new Anime(1L,"DBZZZZZZZZ"),new Anime(2L,"Segundo Anime"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L,"DBZZZZZZZZ"),new Anime(2L,"Segundo Anime")));
+    }
 
     public List<Anime> listAll() {
         return animes;
@@ -21,5 +28,11 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3,10000));
+        animes.add(anime);
+        return anime;
     }
 }
