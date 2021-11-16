074ab4c2d49db54bbca6653f8d35a09ba2fcb871
Gabriel Barros <gabrielbarros201612@gmail.com>
2021-11-16 12:23:16 -0300

    Primeira conexão com banco de dados nas configurações da aplicação, adicionando a tabela Anime ao banco

--- src/main/java/lead/mentoring/springboot2/domain/Anime.java
+++ src/main/java/lead/mentoring/springboot2/domain/Anime.java
@@ -1,13 +1,19 @@
 package lead.mentoring.springboot2.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.ToString;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
