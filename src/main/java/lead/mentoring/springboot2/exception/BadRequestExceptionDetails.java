337cdd8174e2d30c3849a333e2b51c05280efc2b
Gabriel Barros <gabrielbarros201612@gmail.com>
2021-11-18 09:39:21 -0300

    Criando exceção para validação de campos, retornando valores incorretos e mensagens definidas

--- src/main/java/lead/mentoring/springboot2/exception/BadRequestExceptionDetails.java
+++ src/main/java/lead/mentoring/springboot2/exception/BadRequestExceptionDetails.java
@@ -1,20 +1,12 @@
 package lead.mentoring.springboot2.exception;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
-
-@Data
-//Geração de códigos automátiocos com lombok
-@Builder
-//Instanciar objetos pela por uma função de build
-public class BadRequestExceptionDetails {
-
-    private String title;
-    private int status;
-    private String details;
-    private String developerMessage;
-    private LocalDateTime timestamp;
+@Getter
+//Geração de códigos automátiocos com somente getters
+@SuperBuilder
+//Usar o build da super classe
+public class BadRequestExceptionDetails extends ExceptionDetails{
 
 }
--- src/main/java/lead/mentoring/springboot2/handler/RestExceptionHandler.java
+++ src/main/java/lead/mentoring/springboot2/handler/RestExceptionHandler.java
@@ -2,12 +2,17 @@ package lead.mentoring.springboot2.handler;
 
 import lead.mentoring.springboot2.exception.BadRequestException;
 import lead.mentoring.springboot2.exception.BadRequestExceptionDetails;
+import lead.mentoring.springboot2.exception.ValidationExceptionDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 //Interceptador de excessões geradas pelos componentes controllers
@@ -15,7 +20,6 @@ import java.time.LocalDateTime;
 public class RestExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
     //Dado uma determinada execessão lançada, caso corresponda ao tipo passo, vai executar a função a seguir
-
     public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException badRequestException){
         return new ResponseEntity<>(
                 BadRequestExceptionDetails.builder()
@@ -24,6 +28,27 @@ public class RestExceptionHandler {
                         .title("Bad Request Exception, Check the documentation")
                         .details(badRequestException.getMessage())
                         .developerMessage(badRequestException.getClass().getName())
+                        .build(),HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException
+            (MethodArgumentNotValidException methodArgumentNotValidException){
+
+        List<FieldError> fieldErrors = methodArgumentNotValidException.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+        String messages = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Invalid fields")
+                        .details("Simplificando methodArgumentNotValidException.getMessage()")
+                        .developerMessage(methodArgumentNotValidException.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(messages)
                         .build(),HttpStatus.BAD_REQUEST
         );
     }
