10f62529a22a4da08ad4c8981adc273f56e6de98
Gabriel Barros <gabrielbarros201612@gmail.com>
2021-11-22 10:30:15 -0300

    Criando um profile pelo maven para executar os tests de integração e de unidade (mvn test -Pintegration-tests). Alterando o ApplicationTest para não dar erro pela não inicialização do banco de produção

--- src/test/java/lead/mentoring/springboot2/SpringbootEssentialsApplicationTests.java
+++ src/test/java/lead/mentoring/springboot2/SpringbootEssentialsApplicationTests.java
@@ -3,10 +3,10 @@ package lead.mentoring.springboot2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringbootEssentialsApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
